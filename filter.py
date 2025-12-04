f9a99084091a61b392e4aba42f7594b9aba11f88
Lahiru167 <lahirushehan@2002gmail.com>
2025-12-05 01:30:07 +0530

    Add newline and improve readability in filter.py

--- filter.py
+++ filter.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 number =[1,2,3,4,5,6,7,8,9,10]
 
 def even_num(x):
@@ -12,3 +14,6 @@ print(z)
 
 x=list(map(lambda x:x*2,number))
 print(x)
+
+sum = reduce(lambda x,y:x+y,number)
+print(sum)
